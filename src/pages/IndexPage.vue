4ab10aadcc4ac93e00c67d049dabb7e35c0bbb0b
AugustoMagalhaes <augustommg@gmail.com>
2023-06-10 20:20:39 -0300

    style: reseta :active e :visited de .link e cria rota.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,10 +3,15 @@
     <h1 class="main-title">Boas-Vindas</h1>
     <h2 class="secondary-title">
       Toque para Iniciar
-      <q-icon
-        name="arrow_forward_ios"
-        class="arrow-icon border-circle"
-      ></q-icon>
+      <router-link
+        to="/produtos"
+        class="link"
+      >
+        <q-icon
+          name="arrow_forward_ios"
+          class="arrow-icon border-circle"
+        ></q-icon>
+      </router-link>
     </h2>
   </section>
   <footer class="footer">Faça sua recarga pelo App</footer>
@@ -18,6 +23,7 @@
 
 <script>
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
 
 export default defineComponent({
   name: 'IndexPage',
